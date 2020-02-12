06c5765452c76ba002db449e4d55a7f19b6073b9
Richard Parsons <parsons.rk@gmail.com>
2020-02-12 21:14:50 +0100

    Adjustable height for scene

--- src/components/BoxScene/BoxScene.view.tsx
+++ src/components/BoxScene/BoxScene.view.tsx
@@ -2,6 +2,7 @@ import { Canvas, useFrame } from 'react-three-fiber'
 import React, { useRef, useState } from 'react'
 
 import { Mesh } from 'three'
+import { SceneContainer } from './BoxScene.style'
 
 type BoxProps = {
     position: number[]
@@ -35,10 +36,12 @@ const Box = (props: BoxProps) => {
 }
 
 export default () => (
-    <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <SceneContainer>
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    </SceneContainer>
 )
