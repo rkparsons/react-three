ae391e0b03860140fef109c63dedf995b98376d9
Richard Parsons <parsons.rk@gmail.com>
2020-02-19 22:16:52 +0100

    Add index to scene list

--- src/components/SceneGrid/SceneGrid.view.tsx
+++ src/components/SceneGrid/SceneGrid.view.tsx
@@ -15,8 +15,8 @@ export default () => {
     // const [time, setTime] = useState(0)
     // const [speed, setSpeed] = useState(0)
 
-    const scenes = [BoxScene, TextScene, PointScene].map(Component => (
-        <GridItem item>
+    const scenes = [BoxScene, TextScene, PointScene].map((Component, index) => (
+        <GridItem item key={index}>
             <Scene windowHeight={windowSize.height}>
                 {isVisible => <Component isVisible={isVisible} />}
             </Scene>
