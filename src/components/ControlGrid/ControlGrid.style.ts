56296668b788a4808f35ab5acdc8a48884770a87
Richard Parsons <parsons.rk@gmail.com>
2020-02-20 17:46:18 +0100

    Fade in controls

--- src/components/ControlGrid/ControlGrid.style.ts
+++ src/components/ControlGrid/ControlGrid.style.ts
@@ -1,5 +1,6 @@
 import { Grid as GridBase, Theme } from '@material-ui/core'
 
+import { Box } from '@material-ui/core'
 import styled from 'styled-components'
 
 type ControlsProps = {
@@ -11,8 +12,19 @@ export const Grid = styled(GridBase)`
     ${({ theme, opacity }: ControlsProps) => ` 
         position: fixed;
         opacity: ${opacity};
+        transition: opacity 0.1s linear;
         z-index: ${opacity === 0 ? 0 : 1000};
         width: 100%;
         bottom: ${theme.spacing(20)};
+  `}
+`
+type FadeInProps = {
+    opacity: number
+}
+
+export const FadeIn = styled(Box)`
+    ${({ opacity }: FadeInProps) => ` 
+        opacity: ${opacity};
+        transition: opacity 2s linear 0.5s;
   `}
 `
--- src/components/ControlGrid/ControlGrid.view.tsx
+++ src/components/ControlGrid/ControlGrid.view.tsx
@@ -1,14 +1,23 @@
-import React, { ReactNode } from 'react'
-
-import { Grid } from './ControlGrid.style'
+import { FadeIn, Grid } from './ControlGrid.style'
+import React, { ReactNode, useEffect, useState } from 'react'
 
 type ViewProps = {
     opacity: number
     children: ReactNode
 }
 
-export default ({ opacity, children }: ViewProps) => (
-    <Grid container spacing={4} justify="center" opacity={opacity}>
-        {children}
-    </Grid>
-)
+export default ({ opacity, children }: ViewProps) => {
+    const [fadeInOpacity, setFadeInOpacity] = useState(0)
+
+    useEffect(() => {
+        setFadeInOpacity(1)
+    }, [])
+
+    return (
+        <FadeIn opacity={fadeInOpacity}>
+            <Grid container spacing={4} justify="center" opacity={opacity}>
+                {children}
+            </Grid>
+        </FadeIn>
+    )
+}
