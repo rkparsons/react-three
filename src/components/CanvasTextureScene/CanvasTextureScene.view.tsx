42d5519b092ad649805d3c0d32e4f0d05423bec7
Richard Parsons <parsons.rk@gmail.com>
2020-03-03 18:47:16 +0100

    Increase canvas resolution

--- src/components/CanvasTextureScene/CanvasTextureScene.view.tsx
+++ src/components/CanvasTextureScene/CanvasTextureScene.view.tsx
@@ -30,21 +30,22 @@ const ImageTexture = ({ position }: ImageTextureProps) => {
     const [geometryRef, geometry] = useResource<BufferGeometry>()
     const [textureRef, texture] = useResource<CanvasTexture>()
 
-    const canvasWidth = 640,
-        canvasHeight = 250,
+    const canvasWidth = 2048,
+        canvasHeight = canvasWidth / 4,
         radiusTop = canvasWidth / (2 * Math.PI),
-        radiusBottom = 1.4 * radiusTop,
+        radiusBottom = 1.2 * radiusTop,
         height = canvasHeight,
         radialSegments = 64,
         heightSegments = 1,
         isOpenEnded = true,
         thetaStart = 0,
-        thetaLength = 2 * Math.PI
+        thetaLength = 2 * Math.PI,
+        scale = 512 / canvasWidth
     const ctx = document.createElement('canvas').getContext('2d')!
     ctx.canvas.width = canvasWidth
     ctx.canvas.height = canvasHeight
     ctx.fillStyle = 'white'
-    ctx.font = '25pt Arial'
+    ctx.font = `${canvasWidth / 19.1}px Arial`
     ctx.textAlign = 'center'
     ctx.textBaseline = 'middle'
     ctx.fillText('MOVING BORDERS MOVING BORDERS', ctx.canvas.width / 2, ctx.canvas.height / 2)
@@ -69,7 +70,7 @@ const ImageTexture = ({ position }: ImageTextureProps) => {
                 ]}
             />
             <canvasTexture ref={textureRef} image={ctx.canvas} minFilter={LinearFilter} />
-            <group scale={[1, 1, 1]} rotation={[0.4, -rotation, 0]}>
+            <group scale={[scale, scale, scale]} rotation={[0.4, -rotation, 0]}>
                 <mesh renderOrder={2} geometry={geometry}>
                     <Material texture={texture} side={FrontSide} />
                 </mesh>
