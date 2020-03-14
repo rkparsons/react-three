845d50c0a73637d58146774fd5ae18a2f15498f6
Richard Parsons <parsons.rk@gmail.com>
2020-03-14 14:25:57 +0100

    Fix dom element prop

--- src/components/CanvasTextureScene/CanvasTextureScene.view.style.tsx
+++ src/components/CanvasTextureScene/CanvasTextureScene.view.style.tsx
@@ -1,21 +1,14 @@
 import { ContainerProps } from 'react-three-fiber/targets/shared/web/ResizeContainer'
-import { Grid } from '@material-ui/core'
 import { Canvas as R3FCanvas } from 'react-three-fiber'
+import React from 'react'
 import styled from 'styled-components'
 
 type CanvasProps = ContainerProps & {
     isHover: boolean
 }
-export const Canvas = styled(R3FCanvas)`
-    ${({ isHover }: CanvasProps) => ` 
+
+export const Canvas = styled(({ isHover, ...rest }: CanvasProps) => <R3FCanvas {...rest} />)`
+    ${({ isHover }: CanvasProps) => `
         cursor: ${isHover ? 'pointer' : 'default'};
     `}
-`
-
-export const TestCanvas = styled.canvas`
-    border: 1px solid white;
-`
-
-export const TestCanvasGrid = styled(Grid)`
-    width: 100%;
 `
